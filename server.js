a2805c015e2142311872efe917b751e97422fa20
unknown <replitpython22@gmail.com>
2025-07-25 18:15:27 +0530

    done

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
